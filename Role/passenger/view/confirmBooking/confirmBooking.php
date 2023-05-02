2ee58fdcf3b3225612c252e5271e7c26b1ee7ccf
MohammadSheakh <mohammad.sheakh@gmail.com>
2023-05-02 23:31:24 +0600

    done

--- Role/passenger/view/confirmBooking/confirmBooking.php
+++ Role/passenger/view/confirmBooking/confirmBooking.php
@@ -10,130 +10,131 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./confirmBooking.css"/>
 </head>
 <body>
 <?php
     // session_start();
         include '../system/navbar/mainNavbar.php';
     ?>
-
+<h2  align="center" class='inputName'>Ticket Booking Details</h2>
     <table align="center">
+        
         <tr>
-            <td> <h2>Ticket Booking Details</h2> <br> </td>
-        </tr>
-        <tr>
-            <td>
+            <td class='inputName'>
             User Name   
             </td>
-            <td>:</td>
-            <td>
+            <td class='inputName'>:</td>
+            <td class='inputName'>
                 <?php 
                     echo $_SESSION["fullName"]   ?? '';
                 ?> 
             </td>
         </tr>
         <tr>
-            <td>
+            <td class='inputName'>
             User Phone Number 
             </td>
-            <td>:</td>
-            <td>
+            <td class='inputName'>:</td>
+            <td class='inputName'>
                 <?php 
                     echo $_SESSION["phoneNo"]   ?? '';
                 ?> 
             </td>
         </tr>
         <tr>
-            <td>
+            <td class='inputName'>
             User Email 
-            </td><td>:</td>
-            <td>
+            </td><td class='inputName'>:</td>
+            <td class='inputName'>
                 <?php 
                     echo $_SESSION["email"]   ?? '';
                 ?> 
             </td>
         </tr>
         <tr>
-            <td>
+            <td class='inputName'>
             User Status 
-            </td><td>:</td>
-            <td>
+            </td><td class='inputName'>:</td>
+            <td class='inputName'> 
             <?php 
                     echo $_SESSION["type"]   ?? '';
                 ?> 
             </td>
         </tr>
 
-        <tr>
-            <td>
+        <!-- // payment successfull houar pore serial number provide korbo ...  -->
+
+        <!-- <tr>
+            <td class='inputName'>
             Ticket Sl. Number 
-            </td><td>:</td>
+            </td><td class='inputName'>:</td>
             <td>
             <?php 
                     echo $_SESSION["ticketSerial"]   ?? '';
                 ?> 
             </td>
-        </tr>
+        </tr> -->
         <tr>
-            <td>
+            <td class='inputName'>
             From 
-            </td><td>:</td>
-            <td>
+            </td><td class='inputName'>:</td>
+            <td class='inputName'>
             <?php 
                     echo $_SESSION["startArea"]   ?? '';
                 ?> 
             </td>
         </tr>
         <tr>
-            <td>
+            <td class='inputName'>
             To 
-            </td><td>:</td>
-            <td>
+            </td><td class='inputName'>:</td>
+            <td class='inputName'>
             <?php 
                     echo $_SESSION["destinationArea"]   ?? '';
                 ?> 
             </td>
         </tr>
         <tr>
-            <td>
+            <td class='inputName'>
             Bus Company Name 
-            </td><td>:</td>
-            <td>
+            </td><td class='inputName'>:</td>
+            <td class='inputName'>
             <?php 
                     $_SESSION["companyName"]  ;
                 ?> 
             </td>
         </tr>
         <tr>
-            <td>
+            <td class='inputName'>
             Root No. 
-            </td><td>:</td>
-            <td><?php 
+            </td><td class='inputName'>:</td>
+            <td class='inputName'><?php 
                     echo $_SESSION["routeId"]   ?? '';
                 ?> </td>
         </tr>
-        <tr>
-            <td>
+        <tr class='inputName'>
+            <td class='inputName'>
             Vehicle Serial No. 
 
-            </td><td>:</td><td><?php 
+            </td><td class='inputName'>:</td><td class='inputName'><?php 
                 ///////////////////////////////////////// eita database theke niye dekhate hobe .. 
                     echo $_SESSION["busId"]   ?? '';
                 ?> </td>
         </tr>
         <tr>
-            <td>
+            <td class='inputName'>
             Date And Time 
-            </td><td>:</td><td><?php 
+            </td><td class='inputName'>:</td><td class='inputName'><?php 
                     echo $_SESSION["departureTime"]   ?? ''; // $_SESSION["arrivalTime"]
                 ?> </td>
         </tr>
         <tr>
-            <td>
+            <td class='inputName'>
                 Seat No. 
             </td>
-            <td>:</td>
-            <td><?php 
+            <td class='inputName'>:</td>
+            <td class='inputName'><?php 
                     // if(isset($_SESSION["seatNoVarA1"])){
                         
                     // }
@@ -144,36 +145,39 @@ session_start();
                 ?> </td>
         </tr>
         <tr>
-            <td>
+            <td class='inputName'>
                 Total Seat 
             </td>
-            <td>:</td>
-            <td><?php 
-                    echo $_SESSION['seatCount']." seat"   ?? '';
+            <td class='inputName'>:</td>
+            <td class='inputName'><?php 
+                    if(isset($_SESSION['seatCount'])){
+                        echo $_SESSION['seatCount']." seat"   ?? '';
+                    }
+                    
                 ?> </td>
         </tr>
         <tr>
-            <td>
+            <td class='inputName'>
                 Per kilometer price  
-            </td><td>:</td><td><?php 
+            </td><td class='inputName'>:</td><td class='inputName'><?php 
                     echo $_SESSION["perKmCost"]." Tk"   ?? '';
                 ?> </td>
         </tr>
         <tr>
-            <td>
+            <td class='inputName'>
                 Total Distance   
             </td>
-            <td>:</td>
-            <td><?php 
+            <td class='inputName'>:</td>
+            <td class='inputName'><?php 
                     echo $_SESSION["distanceCalculation"]." Km"    ?? '';
                 ?> </td>
         </tr>
         <tr>
-            <td>
+            <td class='inputName'>
                 Total Price 
             </td>
-            <td>:</td>
-            <td><?php 
+            <td class='inputName'>:</td>
+            <td class='inputName'><?php 
                     echo $_SESSION['totalPrice']." Tk"  ?? '';
                 ?> </td>
         </tr>
@@ -183,9 +187,9 @@ session_start();
     <div align="center">
         <h4>To Confirm Booking And Get Ticket Serial No. Please Payment First By Online Payment System</h4>
         <br>
-        <button> <a href="../ticketBooking/ticketBooking.php">Go Back</a> </button>
-        <button> <a href="../confirmBooking/confirmBooking.php">Make payment</a> </button>
-        <button> <a href="../confirmBooking/EditBookingInformation.php">Edit Information</a> </button>
+        <button class='cancelBtn'> <a  class='cancelBtn' href="../ticketBooking/ticketBooking.php">Go Back</a> </button>
+        <!-- <button> <a href="../confirmBooking/confirmBooking.php">Make payment</a> </button> -->
+        <!-- <button> <a href="../confirmBooking/EditBookingInformation.php">Edit Information</a> </button> -->
     </div>
 </body>
 </html>
